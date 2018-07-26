2a991433547c80cd4699d89c4e3a410776fd4f41
Francisco Chacon <pacochacon94@gmail.com>
2018-07-26 13:30:15 -0500

    Project base files

---
+++ FormValidator/FormValidator.h
@@ -0,0 +1,18 @@
+//
+//  FormValidator.h
+//  FormValidator
+//
+//  Created by Francisco Chacon on 7/26/18.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FormValidator.
+FOUNDATION_EXPORT double FormValidatorVersionNumber;
+
+//! Project version string for FormValidator.
+FOUNDATION_EXPORT const unsigned char FormValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FormValidator/PublicHeader.h>
+
+
---
+++ FormValidator/FormValidator.swift
@@ -0,0 +1,8 @@
+//
+//  FormValidator.swift
+//  FormValidator
+//
+//  Created by Francisco Chacon on 7/26/18.
+//
+
+import Foundation
